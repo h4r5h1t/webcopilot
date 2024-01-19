90032949e73bf180567b18b6971689426d5203d7
root <root@DESKTOP-68G7GS8>
2024-01-20 02:10:53 +0700

    mr-vill4in:installation_update (https://github.com/h4r5h1t/webcopilot/pull/6) - https://github.com/h4r5h1t/webcopilot/issues/3

--- install.sh
+++ install.sh
@@ -14,19 +14,75 @@ mkdir -p ~/.gf
 mkdir -p ~/wordlists
 mkdir -p ~/wordlists/payloads/
 
-echo "Installing all dependencies"
-cd
-sudo apt-get install git -y 2> /dev/null
-sudo apt-get install python3 -y 2> /dev/null
-sudo apt-get install python3-pip -y 2> /dev/null
-sudo apt-get install ruby -y 2> /dev/null
-sudo apt-get install golang-go -y 2> /dev/null
-sudo apt install snapd -y 2> /dev/null
-sudo apt install cmake -y 2> /dev/null
-sudo apt install jq -y 2> /dev/null
-sudo apt install gobuster -y 2> /dev/null
-sudo snap install chromium 2> /dev/null
-sudo apt-get install -y parallel 2> /dev/null
+echo "Updating the system"
+sudo apt-get update -y 2> /dev/null
+
+echo "Installing all dependencies and checking is installed or not"
+if ! command -v git &> /dev/null
+then
+    echo "git could not be found"
+    sudo apt-get install git -y 2> /dev/null
+fi
+
+if ! command -v python3 &> /dev/null
+then
+    echo "python3 could not be found"
+    sudo apt-get install python3 -y 2> /dev/null
+fi
+
+if ! command -v python3-pip &> /dev/null
+then
+    echo "python3-pip could not be found"
+    sudo apt-get install python3-pip -y 2> /dev/null
+fi
+
+if ! command -v ruby &> /dev/null
+then
+    echo "ruby could not be found"
+    sudo apt-get install ruby -y 2> /dev/null
+fi
+
+if ! command -v golang-go &> /dev/null
+then
+    echo "golang-go could not be found"
+    sudo apt-get install golang-go -y 2> /dev/null
+fi
+
+if ! command -v snapd &> /dev/null
+then
+    echo "snapd could not be found"
+    sudo apt install snapd -y 2> /dev/null
+fi
+
+if ! command -v cmake &> /dev/null
+then
+    echo "cmake could not be found"
+    sudo apt install cmake -y 2> /dev/null
+fi
+
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found"
+    sudo apt install jq -y 2> /dev/null
+fi
+
+if ! command -v gobuster &> /dev/null
+then
+    echo "gobuster could not be found"
+    sudo apt install gobuster -y 2> /dev/null
+fi
+
+if ! command -v chromium &> /dev/null
+then
+    echo "chromium could not be found"
+    sudo snap install chromium 2> /dev/null
+fi
+
+if ! command -v parallel &> /dev/null
+then
+    echo "parallel could not be found"
+    sudo apt-get install -y parallel 2> /dev/null
+fi
 sleep 2s
 clear
 
@@ -52,24 +108,109 @@ cd ~/wordlists/payloads/ && wget https://raw.githubusercontent.com/R0X4R/Garud/m
 sleep 2s
 
 echo "Installing go-lang tools"
+if ! command -v anew &> /dev/null
+then
+    echo "anew could not be found"
 go install -v github.com/tomnomnom/anew@latest 2> /dev/null
+fi
+
+if ! command -v gf &> /dev/null
+then
+    echo "gf could not be found"
 go install -v github.com/tomnomnom/gf@latest 2> /dev/null
+fi
+
+if ! command -v aquatone &> /dev/null
+then
+    echo "aquatone could not be found"
 go install -v github.com/michenriksen/aquatone@latest 2> /dev/null
+fi
+
+if ! command -v assetfinder &> /dev/null
+then
+    echo "assetfinder could not be found"
 go install -v github.com/tomnomnom/assetfinder@latest 2> /dev/null
+fi
+
 ### Switch back to gau by lc (bp0lr/gauplus - discontinued) - Visit here: (https://github.com/lc/gau)
+# if ! command -v gauplus &> /dev/null
+# then
+#    echo "gauplus could not be found"
 # go install -v github.com/bp0lr/gauplus@latest 2> /dev/null
+# fi
+
+if ! command -v gau &> /dev/null
+then
+    echo "gau could not be found"
 go install -v github.com/lc/gau/v2/cmd/gau@latest 2> /dev/null
+fi
+
+if ! command -v httpx &> /dev/null
+then
+    echo "httpx could not be found"
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest 2> /dev/null
-go install -v github.com/owasp-amass/amass/v4/...@latest 2> /dev/null
+fi
+
+if ! command -v amass &> /dev/null
+then
+    echo "amass could not be found"
+go install -v github.com/owasp-amass/amass/v4/...@master 2> /dev/null
+fi
+
+if ! command -v waybackurls &> /dev/null
+then
+    echo "waybackurls could not be found"
 go install -v github.com/tomnomnom/waybackurls@latest 2> /dev/null
+fi
+
+if ! command -v kxss &> /dev/null
+then
+    echo "kxss could not be found"
 go install -v github.com/Emoe/kxss@latest 2> /dev/null
+fi
+
+if ! command -v subjack &> /dev/null
+then
+    echo "subjack could not be found"
 go install -v github.com/haccer/subjack@latest 2> /dev/null
+fi
+
+if ! command -v qsreplace &> /dev/null
+then
+    echo "qsreplace could not be found"
 go install -v github.com/tomnomnom/qsreplace@latest 2> /dev/null
+fi
+
+if ! command -v dnsx &> /dev/null
+then
+    echo "dnsx could not be found"
 go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest 2> /dev/null
+fi
+
+if ! command -v dalfox &> /dev/null
+then
+    echo "dalfox could not be found"
 go install -v github.com/hahwul/dalfox/v2@latest 2> /dev/null
+fi
+
+if ! command -v crlfuzz &> /dev/null
+then
+    echo "crlfuzz could not be found"
 GO111MODULE=on go install -v github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest 2> /dev/null
+fi
+
+if ! command -v nuclei &> /dev/null
+then
+    echo "nuclei could not be found"
 go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest 2> /dev/null
+fi
+
+if ! command -v subfinder &> /dev/null
+then
+    echo "subfinder could not be found"
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest 2> /dev/null
+fi
+
 # cd ~/tools/temp/ && wget https://github.com/owasp-amass/amass/releases/download/v4.2.0/amass_Linux_i386.zip && unzip amass_linux_i386.zip && cd amass_linux_i386 && mv amass /usr/bin/amass 2> /dev/null
 
 cd
